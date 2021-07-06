dbe769f3db8d5a2ff9a61db986a35fe018de5cb2
veera-chitluri <85030841+veera-chitluri@users.noreply.github.com>
2021-07-06 13:35:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace myfirstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2,total;
+            Console.WriteLine("Enter Number 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            total = num1 + num2;
+            Console.WriteLine("{0} + {1} = {2}",num1,num2,total);
+        }
+    }
+}
